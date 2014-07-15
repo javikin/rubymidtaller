4fb6a5181717a5e80922fd20c286ae496e981996
Javier Olan <javikin10@gmail.com>
2014-07-15 18:34:24 -0500

    avances de la clase

--- app.rb
+++ app.rb
@@ -2,8 +2,23 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
 
-get "/" do
-  @title = "Segun taller de RubyMid"
-  erb :"index"
+class Post < ActiveRecord::Base
+
 end
 
+get "/" do
+	@posts = Post.order("created_at DESC")
+  	@title = "Segun taller de RubyMid"
+	erb :"index"
+end
+
+get '/posts/create' do
+	@title = "Nuevo Post"
+	@post = Post.new
+	erb :"posts/create"
+end
+
+get "/posts/:id" do
+	@post = Post.find(params[:id])	
+	erb :"posts/view"
+end
